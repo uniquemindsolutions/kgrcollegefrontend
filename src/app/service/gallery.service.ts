53cad6ef3257ef8a8c029bf3381f19f10fb2b8cf
vamshikrishnachandra <krishnavamshi9381@gmail.com>
2024-09-18 11:46:13 +0530

    Updated dynamic

--- src/app/service/gallery.service.ts
+++ src/app/service/gallery.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
@@ -9,8 +9,11 @@ export class GalleryService {
   public baseUrl = environment.baseUrl;
   constructor(private http:HttpClient) { }
   getgalleryImages(typevalue:any, subtypevalue:any){
+    const headers = new HttpHeaders({
+      'Authorization': 'Token 98bd29055e21fabbde860dc2f1a98fe16ec52b23', // Authorization header
+    });
     // return this.http.get(`http://127.0.0.1:8000/gallery-images/?type=Gallery&sub_type=Image`);
-    return this.http.get(`${this.baseUrl}/gallery-images/?type=Gallery&sub_type=Image`);
+    return this.http.get(`${this.baseUrl}/gallery-images/?type=Gallery&sub_type=Image`,{headers});
   }
   
 }
--- src/app/service/syllabus.service.ts
+++ src/app/service/syllabus.service.ts
@@ -1,19 +1,17 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class EventsandActivitesService {
+export class SyllabusService {
   public baseUrl = environment.baseUrl;
 
-
   constructor(private http:HttpClient) { }
-  geteventsandactivitesImages(typevalue:any, subtypevalue:any){
+  getSyllabus(){
     const headers = new HttpHeaders({
       'Authorization': 'Token 98bd29055e21fabbde860dc2f1a98fe16ec52b23', // Authorization header
     });
-    // return this.http.get(`http://127.0.0.1:8000/eventsandactivites/?type=EventsandActivites&sub_type=Image`);
-    return this.http.get(`${this.baseUrl}/eventsandactivites/?type=EventsandActivites&sub_type=Image`,{headers});
+    return this.http.get(`${this.baseUrl}/syllabus/`,{headers})
   }
 }
